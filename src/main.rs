1d90f7484bcebaf9a34cfb71258f8a500929034d
Nick Tzaperas <nick@nijotz.com>
2016-04-04 23:08:38 -0700

    Movement

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ impl Actor {
     fn new() -> Actor {
         let p: Vec2<f64> = Vec2::new(0f64, 0f64);
         let v: Vec2<f64> = Vec2::new(0f64, 0f64);
-        let a: Vec2<f64> = Vec2::new(0f64, 0f64);
+        let a: Vec2<f64> = Vec2::new(1f64, 1f64);
         Actor {
             position: p,
             velocity: v,
@@ -28,11 +28,20 @@ impl Actor {
         self.acceleration = self.acceleration + force;
     }
 
-    fn draw(&self) {
-        println!("position: {}", self.position);
+    fn render(&self, gl: &mut GlGraphics, args: &RenderArgs) {
+        use graphics::*;
+        let square = rectangle::square(0.0, 0.0, 50.0);
+        const RED:   [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+        let rotation = 0.0;
+        gl.draw(args.viewport(), |c, gl| {
+            let transform = c.transform.trans(self.position.x, self.position.y)
+                                       .rot_rad(rotation)
+                                       .trans(-25.0, -25.0);
+            rectangle(RED, square, transform, gl)
+        })
     }
 
-    fn update(&mut self) {
+    fn update(&mut self, args: &UpdateArgs) {
         self.velocity = self.velocity + self.acceleration * SPT;
         self.position = self.position + self.velocity * SPT;
     }
@@ -51,7 +60,7 @@ use opengl_graphics::{ GlGraphics, OpenGL };
 
 pub struct World {
     gl: GlGraphics,
-    //actors: Vec<Actor>
+    actors: Vec<Actor>
 }
 
 impl World {
@@ -61,10 +70,17 @@ impl World {
         self.gl.draw(args.viewport(), |c, gl| {
             // Clear the screen.
             clear(BLACK, gl);
-        })
+        });
+
+        for actor in &self.actors {
+            actor.render(&mut self.gl, args);
+        }
     }
 
     fn update(&mut self, args: &UpdateArgs) {
+        for actor in self.actors.iter_mut() {
+            actor.update(args);
+        }
     }
 }
 
@@ -83,11 +99,16 @@ fn main() {
         .unwrap();
 
     let mut world = World {
-        gl: GlGraphics::new(opengl)
+        gl: GlGraphics::new(opengl),
+        actors: Vec::new()
     };
 
     let mut events = window.events();
     while let Some(e) = events.next(&mut window) {
+        if let Some(button) = e.press_args() {
+            world.actors.push(Actor::new());
+        }
+
         if let Some(r) = e.render_args() {
             world.render(&r);
         }
