ea8d8efa1a6b809b4b880fb758b010ce76ad705c
Nick Tzaperas <nick@nijotz.com>
2016-04-11 17:09:49 -0700

    Test for circular dependencies

--- tests/lib.rs
+++ tests/lib.rs
@@ -23,6 +23,22 @@ mod tests {
         let outputs = genome.network.evaluate(inputs);
 
         assert!(outputs.len() == 1);
-        assert!(outputs[0] > 0f64 && outputs[0] < 1f64);
+        assert!(outputs[0] >= 0f64 && outputs[0] <= 1f64);
+    }
+
+    #[test]
+    fn it_handles_circular_dependencies() {
+        let _ = env_logger::init();
+        let genome = Genome::new(vec![
+            Gene{ into: 1, out: 3, weight: 1.0, enabled: true },
+            Gene{ into: 2, out: 3, weight: 1.0, enabled: true },
+            Gene{ into: 3, out: 2, weight: 1.0, enabled: true }
+        ], 2, 1);
+
+        let inputs = vec![1f64, 1f64];
+        let outputs = genome.network.evaluate(inputs);
+
+        assert!(outputs.len() == 1);
+        assert!(outputs[0] >= 0f64 && outputs[0] <= 1f64);
     }
 }
