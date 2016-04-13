f965830414e4e4b877dad5e3e0f944ab590fca39
Nick Tzaperas <nick@nijotz.com>
2016-04-13 14:44:54 -0700

    Breeding tests

--- tests/lib.rs
+++ tests/lib.rs
@@ -14,9 +14,9 @@ mod tests {
     fn it_works() {
         let _ = env_logger::init();
         let genome = Genome::new(vec![
-            Gene{ into: 0, out: 3, weight: 1.0, enabled: true },
-            Gene{ into: 1, out: 3, weight: 1.0, enabled: true },
-            Gene{ into: 3, out: 2, weight: 1.0, enabled: true }
+            Gene{ into: 0, out: 3, weight: 1.0, enabled: true, innovation: 1 },
+            Gene{ into: 1, out: 3, weight: 1.0, enabled: true, innovation: 2 },
+            Gene{ into: 3, out: 2, weight: 1.0, enabled: true, innovation: 3 }
         ], 2, 1);
 
         let inputs = vec![1f64, 1f64];
@@ -30,9 +30,9 @@ mod tests {
     fn it_handles_circular_dependencies() {
         let _ = env_logger::init();
         let genome = Genome::new(vec![
-            Gene{ into: 1, out: 3, weight: 1.0, enabled: true },
-            Gene{ into: 2, out: 3, weight: 1.0, enabled: true },
-            Gene{ into: 3, out: 2, weight: 1.0, enabled: true }
+            Gene{ into: 1, out: 3, weight: 1.0, enabled: true, innovation: 1 },
+            Gene{ into: 2, out: 3, weight: 1.0, enabled: true, innovation: 2 },
+            Gene{ into: 3, out: 2, weight: 1.0, enabled: true, innovation: 3 }
         ], 2, 1);
 
         let inputs = vec![1f64, 1f64];
@@ -40,5 +40,25 @@ mod tests {
 
         assert!(outputs.len() == 1);
         assert!(outputs[0] >= 0f64 && outputs[0] <= 1f64);
+    }
+
+    #[test]
+    fn it_breeds() {
+        let _ = env_logger::init();
+        let genome1 = Genome::new(vec![
+            Gene{ into: 0, out: 3, weight: 1.0, enabled: true, innovation: 1 },
+            Gene{ into: 1, out: 3, weight: 1.0, enabled: true, innovation: 2 },
+            Gene{ into: 3, out: 2, weight: 1.0, enabled: true, innovation: 3 }
+        ], 2, 1);
+
+        let genome2 = Genome::new(vec![
+            Gene{ into: 1, out: 3, weight: 1.0, enabled: true, innovation: 1 },
+            Gene{ into: 2, out: 3, weight: 1.0, enabled: true, innovation: 2 },
+            Gene{ into: 3, out: 2, weight: 1.0, enabled: true, innovation: 3 }
+        ], 2, 1);
+
+        let child = genome1.breed(&genome2);
+
+        assert!(child.genes.len() > 0);
     }
 }
