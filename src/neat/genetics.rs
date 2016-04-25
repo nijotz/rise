2e1856f5d7ed495439377b1315a4fdca85e05931
Nick Tzaperas <nick@nijotz.com>
2016-04-25 16:09:14 -0700

    Test innovation order preservation

--- src/neat/genetics.rs
+++ src/neat/genetics.rs
@@ -286,4 +286,34 @@ mod tests {
 
         assert!(child.genes.len() > 0);
     }
+
+    #[test]
+    fn genome_breeding_preserves_innovation_ordering() {
+        // TODO: Randomly fails, need a seeded rng in genetics module
+        let genome1 = Genome::new(vec![
+            Gene{ into: 0, out: 3, weight: 1.0, enabled: true, innovation: 1 },
+            Gene{ into: 1, out: 3, weight: 1.0, enabled: true, innovation: 2 },
+            Gene{ into: 3, out: 2, weight: 1.0, enabled: true, innovation: 3 }
+        ], 2, 1);
+
+        let genome2 = Genome::new(vec![
+            Gene{ into: 1, out: 3, weight: 1.0, enabled: true, innovation: 4 },
+            Gene{ into: 2, out: 3, weight: 1.0, enabled: true, innovation: 5 },
+            Gene{ into: 3, out: 2, weight: 1.0, enabled: true, innovation: 6 }
+        ], 2, 1);
+
+        let child = genome1.breed(&genome2);
+        println!("Child: {:?}", child);
+        let mut ordered = true;
+        let mut innovation: u64 = 0;
+        for gene in child.genes.iter() {
+            if innovation >= gene.innovation {
+                ordered = false;
+                break;
+            }
+
+            innovation = gene.innovation;
+        }
+        assert!(ordered);
+    }
 }
